7fb7f13d69bc5497df68a1fbc3a1325f4ead410f
KawagoeZoo <lifestyle20161116@gmail.com>
2018-11-28 23:37:20 +0800

    init

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  changeCount(step) {
+    this.setState(prevState => ({
+      count: prevState.count + step
+    }));
+  }
+
+  increase = () => {
+    this.changeCount(9);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Count: {this.state.count}</Text>
+        <Button title="increase" onPress={this.increase} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
